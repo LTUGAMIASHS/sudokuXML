b3c3e1200da74e4259187ce927f683b7bf35455e
Laurent THEVENON <laurent.thevenon@etu-grenoble.alpes.fr>
2026-01-01 17:35:10 +0100

    add annexe

---
+++ Rendu_Final/Annexe/gen_full_sudoku_with_attributes.py
@@ -0,0 +1,12 @@
+for row in range(9):
+    res1 = "\t<cell row=\"" + f"{row}"
+    for col in range(9):
+        if (row < 3):
+            zone = 0
+        elif (row < 6):
+            zone = 3
+        else:
+            zone = 6
+        zone += col // 3
+        res = res1 + "\" col=\"" + f"{col}" + "\" zone=\"" + f"{zone}" + "\"></cell>"
+print(res)
